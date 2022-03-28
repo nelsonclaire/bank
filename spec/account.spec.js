01e770e33a8a3d5db0a199a8b4ec89ab2bfcc8e7
Claire Nelson <claire.nelson@johnlewis.co.uk>
2022-03-28 13:13:59 +0100

    First test added to open an account with zero balance

---
+++ spec/account.spec.js
@@ -0,0 +1,17 @@
+const Account = require('./account')
+
+describe('Account', () => {
+  let account
+
+  beforeEach(() => {
+    account = new Account()
+  })
+
+  describe('the initialisation of a new bank account', () => {
+    
+    it('initialises with a balance of zero', () => {
+      expect(account.balance).toEqual(0)
+    })
+
+  })
+})
